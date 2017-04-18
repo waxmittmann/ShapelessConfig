e8bdff4c72da6f631ceb57a43e7939264665cfb1
Maximilian Wittmann <Maximilian.Wittmann@cba.com.au>
2017-04-18 16:15:35 +1000

    Factor out base methods from FromMapConfig and friends to FromConfig. Implementing this will allow new input types (ie scalatypesafe config)

--- src/main/scala/shapelessconfig/FromMapConfig.scala
+++ src/main/scala/shapelessconfig/FromMapConfig.scala
@@ -2,18 +2,39 @@ package shapelessconfig
 
 import scala.util.Try
 
-import shapeless.{::, HList, HNil}
+import shapeless.{::, HNil}
 import cats.syntax.all._
 
 /**
   * Components for reading config from nested maps.
   *
-  * To use, 'import MapConfig._'
+  * To use, 'import FromMapConfig._'
   */
-case class MapConfigParserBuilder[S](key: String, t: String => Either[Err, S]) extends ConfigParserBuilder[Map[String, _]] {
-  type OUT = S :: HNil
+trait FromConfig[I] { self =>
+  type INPUT_SELECT = String
+  type INPUT = I
 
-  override def read(in: Map[String, _]): Either[Err, OUT] =
+  def read[S](in: INPUT, key: String, t: String => Either[Err, S]): Either[Err, S]
+
+  def unit[S](key: String, t: String => Either[Err, S]): ConfigParserBuilder[INPUT] { type OUT = S :: HNil }
+
+  def int(key: String): ConfigParserBuilder[INPUT] { type OUT = Int :: HNil }
+
+  def string(key: String): ConfigParserBuilder[INPUT] { type OUT = String :: HNil }
+
+  implicit def narrow(in: Map[String, _], key: String): Either[Err, Map[String, _]]
+
+
+  case class ConfigParserBuilderInstance[S](key: String, t: String => Either[Err, S]) extends ConfigParserBuilder[INPUT] {
+    type OUT = S :: HNil
+
+    override def read(in: INPUT): Either[Err, OUT] =
+      self.read(in, key, t).map(_ :: HNil)
+  }
+}
+
+object FromMapConfig extends FromConfig[Map[String, _]] {
+  override def read[S](in: Map[String, _], key: String, t: String => Either[Err, S]): Either[Err, S] =
     for {
       rv   <- in.get(key).toRight(Err(s"Map did not contain $key"))
 
@@ -23,34 +44,24 @@ case class MapConfigParserBuilder[S](key: String, t: String => Either[Err, S]) e
       v = rv.asInstanceOf[String]
 
       cv  <- t(v)
-    } yield {
-      println(cv)
-      HList(cv)
-    }
-}
+    } yield
+      cv
 
-trait MapConfigParserBuilderSyntax {
-  def unit[S](key: String, t: String => Either[Err, S]): MapConfigParserBuilder[S] { type OUT = S :: HNil } =
-    MapConfigParserBuilder(key, t)
+  def unit[S](key: String, t: String => Either[Err, S]): ConfigParserBuilderInstance[S] { type OUT = S :: HNil } =
+    ConfigParserBuilderInstance[S](key, t)
 
-  def int(key: String): MapConfigParserBuilder[Int] { type OUT = Int :: HNil } =
-    MapConfigParserBuilder(key, i => Try(i.toInt).toEither.left.map(t => Err(s"Failed to parse to int: ${t.getMessage}")))
+  def int(key: String): ConfigParserBuilderInstance[Int] { type OUT = Int :: HNil } = {
+    ConfigParserBuilderInstance[Int](key, i => Try(i.toInt).toEither.left.map(t => Err(s"Failed to parse to int: ${t.getMessage}")))
+  }
 
-  def string(key: String): MapConfigParserBuilder[String] { type OUT = String :: HNil } =
-    MapConfigParserBuilder(key, Right(_))
+  def string(key: String): ConfigParserBuilderInstance[String] { type OUT = String :: HNil } =
+    ConfigParserBuilderInstance[String](key, Right(_))
 
   implicit def narrow(in: Map[String, _], key: String): Either[Err, Map[String, _]] =
     for {
       v <- in.get(key).toRight(Err(s"Key $key not found in $in"))
       newI <- Try(v.asInstanceOf[Map[String, _]]).toEither.left
-                .map(t => Err(s"Failed to cast ${v.getClass} to Map[String, _].\n${t.getMessage}"))
+        .map(t => Err(s"Failed to cast ${v.getClass} to Map[String, _].\n${t.getMessage}"))
     } yield
       newI
-}
-
-object FromMapConfig extends MapConfigParserBuilderSyntax {
-  type INPUT_SELECT = String
-  type INPUT = Map[INPUT_SELECT, _]
-
-  implicit val configParserBuilder = MapConfigParserBuilder
 }
